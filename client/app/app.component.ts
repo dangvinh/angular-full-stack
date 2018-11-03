af350494b00afcaf9cb2515affdfea4758a1f2b2
VinhDang <30401708+dangvinh@users.noreply.github.com>
2018-11-03 23:00:58 +0700

    intitial

---
+++ client/app/app.component.ts
@@ -0,0 +1,20 @@
+import { AfterViewChecked, ChangeDetectorRef, Component } from '@angular/core';
+import { AuthService } from './services/auth.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements AfterViewChecked {
+
+	constructor(
+  		public auth: AuthService,
+    	private changeDetector: ChangeDetectorRef
+  	) { }
+
+	ngAfterViewChecked() {
+	    this.changeDetector.detectChanges();
+	}
+
+}
